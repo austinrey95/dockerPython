06bfc997a89b27710cb685e64e1bbce755f6b2c8
austinrey95 <36275927+austinrey95@users.noreply.github.com>
2018-03-26 11:42:08 -0500

    Update simpleapp.py

--- simpleapp.py
+++ simpleapp.py
@@ -1,22 +1,9 @@
-from flask import Flask
-import sys
-import optparse
-import time
-
+From flask import Flask
 app = Flask(__name__)
 
-start = int(round(time.time()))
-
-@app.route("/")
+@app.route('/')
 def hello_world():
-
-    return "Hello world from Distelli & Docker!"
+    return 'Flask Dockerized'
 
 if __name__ == '__main__':
-    parser = optparse.OptionParser(usage="python simpleapp.py -p ")
-    parser.add_option('-p', '--port', action='store', dest='port', help='The port to listen on.')
-    (args, _) = parser.parse_args()
-    if args.port == None:
-        print "Missing required argument: -p/--port"
-        sys.exit(1)
-    app.run(host='0.0.0.0', port=int(args.port), debug=False)
+    app.run(debug=True,host='0.0.0.0')
